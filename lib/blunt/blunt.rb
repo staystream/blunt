355d5f20639203db27d746951c1586ea6fa51532
Jamie Brough <jamiebrough@brough.local>
2009-04-16 18:03:36 +0100

    pseudo spatial predicates for ruby Range

--- lib/blunt/blunt.rb
+++ lib/blunt/blunt.rb
@@ -1,17 +1,19 @@
 module Blunt
-  class Range
+  class Ranges
 
-    attr_accessor :ranges
+    attr_accessor :ranges, :widows
 
     def initialize(d, specified_options={})
       
       default_options = {:day   => false,
                          :days  => 7,
                          :time  => false}
-      
+            
       options = default_options.merge specified_options
       options[:days] = 7 if options[:days] > 7
       
+      @widows = []
+
       case d.class.to_s
       when "Range"
         a, b = [d.first, d.last].map {|it| Date.parse(it.strftime("%Y/%m/%d"))}
@@ -43,17 +45,20 @@ module Blunt
       end.map {|it| it.first..it.last}
     end
 
-    def range_by_wday(range,day,days)
+    def range_by_wday(range, day, days)
       chunks = []
       range.each do |it| 
         next unless it.wday == day
         chunk = []
         days.times do |i| 
-          d = it+i
+          d = it + i
           chunk << d if range.include?(d)
         end
-        chunks << (chunk.first..chunk.last)
+        chunks << (chunk.first..chunk.last) unless chunk.size < days
       end
+      return chunks unless chunks.any?
+      @widows << (range.first < chunks.first.first ? (range.first..(chunks.first.first - 1)) : nil)
+      @widows << (range.last > chunks.last.last ? ((chunks.last.last + 1)..range.last) : nil)
       chunks
     end
   end # Range
@@ -64,5 +69,18 @@ module Blunt
     end
   end
 
+  module RangeExtensions
+    def st_intersects?(it)
+      it = it..it unless it.is_a?(Range)
+      (it.first <= first && it.last > first) || ( it.first >= first && it.first < last) || (it.last == last) || self.include?(it.first)
+    end
+
+    def st_intersection(it)
+      return nil unless self.st_intersects?(it)
+      Blunt::Ranges.new(self.select {|n| it.st_intersects?(n) }).ranges.first
+    end
+  end
+
   Object.send :include, Blunt::ObjectExtensions
+  Object.send :include, Blunt::RangeExtensions
 end # Blunt
